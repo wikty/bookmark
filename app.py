b2e2166b91e3f224d04386e9949b8fdf0e9e55fa
wikty <wiktymouse@gmail.com>
2014-07-30 20:24:19 +0800

    reconstruct app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import psycopg2
 from flask import Flask
 from flask_peewee.db import Database
 
-
 app = Flask(__name__)
 app.config.from_object('config.Configuration')
+app.config['SECRET_KEY'] =  open('.secret_key', 'rb').read()
 db = Database(app)
